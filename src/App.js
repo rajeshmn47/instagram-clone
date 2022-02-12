1fe7dee01760b0ca0d7a782be85405971f87d606
rajeshmn47 <rajeshmn47@gmail.com>
2022-02-12 09:13:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <main class="max-w-4xl mx-auto">
+
+  <div class="flex">
+<p class="m-5 text-green-900 ">
+  Bender, quit destroying the universe! Yeah, I do that with my stupidness. I never loved you. Moving along…
+  Belligerent and numerous.
+</p>
+<button class="text-white m-5 font-bold bg-green-900 hover:bg-purple-800 py-2 px-8 rounded">
+  Party with Slurm!
+</button>
+</div>
+<ol class="list-decimal list-inside my-5 pl-2">
+  <li>rajesh</li>
+  <li>rajesh</li>
+  <li>rajesh</li>
+  <li>rajesh</li>
+</ol>
+<ol>
+  <li>rajesh</li>
+  <li>rajesh</li>
+  </ol>
+  </main>
+  <main class="max-w-4xl mx-auto">
+  <div className="text-gray-700 text-center flex items-center align-items">
+    <div>
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh     i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh     i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh </div>
+    <div>
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh     i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
     </div>
+    <div>
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh     i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+  
+  </div><div>
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+  
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+    i am rajesh   i am rajesh   i am rajesh  i am rajesh 
+  </div>
+    </div>
+    </main>
+  </>
   );
 }
 
